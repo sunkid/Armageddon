66fca17a23b78ae757a5e3d97b0b9cc79e419eed
sunkid <sunkid@iminurnetz.com>
2011-06-07 13:45:38 -0700

    skeleton

---
+++ src/main/java/com/iminurnetz/bukkit/plugin/cannonball/CannonBallPlugin.java
@@ -0,0 +1,12 @@
+package com.iminurnetz.bukkit.plugin.cannonball;
+
+import com.iminurnetz.bukkit.plugin.BukkitPlugin;
+
+public class CannonBallPlugin extends BukkitPlugin {
+
+    @Override
+    public void enablePlugin() throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+}
